148e9e5cb7b46de82e1cb1b29dc730b167a6a67d
Weverton <weverton-matias@hotmail.com>
2019-06-03 22:55:51 -0300

    voltando para o banco sqlite, importando as imagens do bruno, adicionando logo nova,e sincronizando as paginas de parceiros e quem somos.

--- projeto/settings.py
+++ projeto/settings.py
@@ -73,14 +73,18 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'xe',
-        'USER': 'system',
-        'PASSWORD': '185723',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+
+#    'default': {
+#    'ENGINE': 'django.db.backends.oracle',
+#    'NAME': 'xe',
+#   'USER': 'system',
+#    'PASSWORD': '185723',
+#     'HOST': '',
+#    'PORT': '',
     }
 }
 
